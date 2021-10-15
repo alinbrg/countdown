8feb788de9c696bfd49bd721062a3aec280a6722
Alina Bregvadze <82210320+alinbrg@users.noreply.github.com>
2021-10-15 21:12:07 +0400

    added files
    added completed files

---
+++ main.js
@@ -0,0 +1,22 @@
+function updateCountDown(){
+	const countDay = new Date("Oct 10, 2022 00:00:00").getTime();
+	const now = new Date().getTime();
+	const gap = countDay - now;
+
+	const second = 1000;
+	const minute = second * 60;
+	const hour = minute * 60;
+	const day = hour * 24;
+
+	const textDay = Math.floor(gap / day);
+	const textHour = Math.floor((gap % day) / hour);
+	const textMinute = Math.floor((gap % hour) / minute);
+	const textSecond = Math.floor((gap % minute) / second);
+
+	document.getElementById('days').innerText = textDay;
+	document.getElementById('hours').innerText = textHour;
+	document.getElementById('minutes').innerText = textMinute;
+	document.getElementById('seconds').innerText = textSecond;
+}
+
+setInterval(updateCountDown, 1000);
